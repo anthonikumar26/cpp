5d46d5e5e5b46e9879b83cc5852a9bff3a41f8f0
Anthoni Kumar <52827570+anthonikumar26@users.noreply.github.com>
2020-12-06 09:45:21 +0530

    Update 1_virtual_destructor.cpp

--- cpp_interview_questions/1_virtual_destructor.cpp
+++ cpp_interview_questions/1_virtual_destructor.cpp
@@ -1,1 +1,38 @@
 
+#include <iostream>
+
+using namespace std;
+
+class Base {
+    virtual void method() {
+      cout << "Base" << endl;
+    }
+  public:
+    virtual ~Base() {
+      method();
+    }
+};
+
+class Derived : public Base {
+    void method() {
+      cout << "Derived"<< endl;
+    }
+  public:
+    ~ Derived() {
+      method();
+    }
+};
+
+int main(void) {
+  Base *base = new Derived;
+  delete base;
+  return 0;
+}
+
+
+/*---output---
+
+Derived
+Base
+
+*/
